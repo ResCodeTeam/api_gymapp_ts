b2a585a429499bbd37ed5043050a0ae9f7f775aa
Fátima <a18834@alunos.ipca.pt>
2022-03-25 10:44:25 +0000

    Alterar encerrar desafio service

---
+++ src/api/services/admin/desafios/encerrarDesafiosService.ts
@@ -0,0 +1,34 @@
+import { client } from "../../../prisma/client";
+import { checkUserIdExists } from "../../../helpers/dbHelpers";
+
+interface IDesafio{
+    estado : number
+    desafioId : string
+}
+
+export class EncerrarDesafiosService {
+    async execute({estado, desafioId} : IDesafio){
+
+        if (desafioId == null) {
+            throw new Error("Impossível encerrar desafio.");
+        }
+
+        if (estado == -1) {
+            await client.desafios.update({
+                where : {
+                    desafio_id : desafioId
+                },
+                data: {
+                    estado
+                }
+            });
+            return {
+                message: "Desafio encerrado com sucesso!"
+            }
+        }
+
+        return {
+            message: "Impossível encerrar desafio."
+        }
+    }
+}
