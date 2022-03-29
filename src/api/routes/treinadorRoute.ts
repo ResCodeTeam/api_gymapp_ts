61e00656de120064892ce438f5c70482283d1917
Fátima <a18834@alunos.ipca.pt>
2022-03-29 12:31:41 +0100

    Treinador conseguir exercícios criados por todos os treinadores.
    - Como treinador quero conseguir obter os exercícios criados por todos os treinadores.

--- src/api/routes/treinadorRoute.ts
+++ src/api/routes/treinadorRoute.ts
@@ -4,13 +4,19 @@ const treinadorRouter = express.Router();
 //Imports
 import { verificarAutenticacao } from "../middlewares/verificarAutenticacao";
 import { CriarComentarioController } from "../controllers/treinador/comentarios/criarComentarioController";
+import { VerTodosOsExerciciosTreinadoresController } from "../controllers/treinador/Exercicios/VerTodosOsExerciciosTreinadoresController";
 
 
 //
 const criarComentarioController = new CriarComentarioController();
+const verTodosOsExerciciosTreinadoresController = new VerTodosOsExerciciosTreinadoresController;
 
 //#region Comentarios
 treinadorRouter.post("/posts/:id/comentarios/",verificarAutenticacao, criarComentarioController.handle);
+//#endregion
+
+//#region Exercicios
+treinadorRouter.get("/exercicios/treinador", verTodosOsExerciciosTreinadoresController.handle);
 //#endregion
 
 export { treinadorRouter };
