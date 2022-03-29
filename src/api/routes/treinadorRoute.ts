3c7576419ff547d9ea1667412c224b399d6cf7e4
Fátima <a18834@alunos.ipca.pt>
2022-03-29 14:41:38 +0100

    Ver todos os treinos dos alunos

--- src/api/routes/treinadorRoute.ts
+++ src/api/routes/treinadorRoute.ts
@@ -4,12 +4,14 @@ const treinadorRouter = express.Router();
 //Imports
 import { verificarAutenticacao } from "../middlewares/verificarAutenticacao";
 import { CriarComentarioController } from "../controllers/treinador/comentarios/criarComentarioController";
-import { VerTodosOsExerciciosTreinadoresController } from "../controllers/treinador/Exercicios/VerTodosOsExerciciosTreinadoresController";
+import { VerTodosOsExerciciosTreinadoresController } from "../controllers/treinador/exercicios/VerTodosOsExerciciosTreinadoresController";
+import { VerTodosTreinosDosAlunosController } from "../controllers/treinador/treinos/verTodosTreinosDosAlunosController";
 
 
 //
 const criarComentarioController = new CriarComentarioController();
-const verTodosOsExerciciosTreinadoresController = new VerTodosOsExerciciosTreinadoresController;
+const verTodosOsExerciciosTreinadoresController = new VerTodosOsExerciciosTreinadoresController();
+const verTodosTreinosDosAlunosController = new VerTodosTreinosDosAlunosController();
 
 //#region Comentarios
 treinadorRouter.post("/posts/:id/comentarios/",verificarAutenticacao, criarComentarioController.handle);
@@ -18,6 +20,11 @@ treinadorRouter.post("/posts/:id/comentarios/",verificarAutenticacao, criarComen
 //#region Exercicios
 treinadorRouter.get("/exercicios/treinador", verTodosOsExerciciosTreinadoresController.handle);
 //#endregion
+
+//#region Treinos
+treinadorRouter.get("/treinos/treinador", verTodosTreinosDosAlunosController.handle);
+//#endregion
+
 
 export { treinadorRouter };
 
