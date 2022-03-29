7c8ea5a30b254672fd1542e9372515971d2337c3
Fátima <a18834@alunos.ipca.pt>
2022-03-29 15:28:13 +0100

    Editar e Remover Desafio Treinador
    -Editar desafio
    -Remover desafio

--- src/api/routes/treinadorRoute.ts
+++ src/api/routes/treinadorRoute.ts
@@ -6,25 +6,33 @@ import { verificarAutenticacao } from "../middlewares/verificarAutenticacao";
 import { CriarComentarioController } from "../controllers/treinador/comentarios/criarComentarioController";
 import { VerTodosOsExerciciosTreinadoresController } from "../controllers/treinador/exercicios/VerTodosOsExerciciosTreinadoresController";
 import { VerTodosTreinosDosAlunosController } from "../controllers/treinador/treinos/verTodosTreinosDosAlunosController";
+import { EditarDesafioController } from "../controllers/treinador/desafios/EditarDesafioController";
+import { RemoverDesafioController } from "../controllers/treinador/desafios/removerDesafioController";
 
 
 //
 const criarComentarioController = new CriarComentarioController();
 const verTodosOsExerciciosTreinadoresController = new VerTodosOsExerciciosTreinadoresController();
 const verTodosTreinosDosAlunosController = new VerTodosTreinosDosAlunosController();
+const editarDesafio = new EditarDesafioController()
+const removerDesafio = new RemoverDesafioController()
 
 //#region Comentarios
 treinadorRouter.post("/posts/:id/comentarios/",verificarAutenticacao, criarComentarioController.handle);
 //#endregion
 
 //#region Exercicios
-treinadorRouter.get("/exercicios/treinador", verTodosOsExerciciosTreinadoresController.handle);
+treinadorRouter.get("/exercicios/", verTodosOsExerciciosTreinadoresController.handle);
 //#endregion
 
 //#region Treinos
-treinadorRouter.get("/treinos/treinador", verTodosTreinosDosAlunosController.handle);
+treinadorRouter.get("/treinos/", verTodosTreinosDosAlunosController.handle);
 //#endregion
 
+//#region Desafios
+treinadorRouter.put("/desafio/:id", editarDesafio.handle);
+treinadorRouter.delete("/desafio/:id", removerDesafio.handle);
+//#endregion
 
 export { treinadorRouter };
 
