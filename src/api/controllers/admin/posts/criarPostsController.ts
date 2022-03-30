2881917b787d6182e8eb0ee04a83616cd25a6bae
“EngenheiroHenrique” <a21122@alunos.ipca.pt>
2022-03-30 15:43:11 +0100

    ggj

--- src/api/controllers/admin/posts/criarPostsController.ts
+++ src/api/controllers/admin/posts/criarPostsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CriarPostsService } from "../../../services/admin/posts/criarPostsService";
+import { CriarPostsService } from "../../../services/treinador/posts/criarPostsService";
 
 class CriarPostsController {
   async handle(request: Request, response: Response) {
