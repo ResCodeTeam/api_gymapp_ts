63d999ffa12cb7349b95e0dfcf82861558007fb4
Fátima <a18834@alunos.ipca.pt>
2022-03-30 17:23:12 +0100

    Como treinador editar e remover avaliação
    - Editar Avaliação
    - Remover Avaliaçao
    - Erros resolvidos em editar um desafio, este não verificava se existia um desafio

--- src/api/routes/treinadorRoute.ts
+++ src/api/routes/treinadorRoute.ts
@@ -5,11 +5,12 @@ const treinadorRouter = express.Router();
 import { verificarAutenticacao } from "../middlewares/verificarAutenticacao";
 import { CriarComentarioController } from "../controllers/treinador/comentarios/criarComentarioController";
 import { VerTodosTreinosDosAlunosController } from "../controllers/treinador/treinos/verTodosTreinosDosAlunosController";
-import { EditarDesafioController } from "../controllers/treinador/desafios/EditarDesafioController";
+import { EditarDesafioController } from "../controllers/treinador/desafios/editarDesafioController";
 import { RemoverDesafioController } from "../controllers/treinador/desafios/removerDesafioController";
 import { CriarDesafioController } from "../controllers/treinador/desafios/criarDesafioController";
 import { VerTodosOsExerciciosTreinadoresController } from "../controllers/treinador/Exercicios/VerTodosOsExerciciosTreinadoresController";
-
+import { RemoverAvaliacaoController } from "../controllers/treinador/avaliacoes/RemoverAvaliacaoController";
+import { EditarAvaliacaoController } from "../controllers/treinador/avaliacoes/editarAvaliacaoController";
 
 //
 const criarComentarioController = new CriarComentarioController();
@@ -18,6 +19,8 @@ const verTodosTreinosDosAlunosController = new VerTodosTreinosDosAlunosControlle
 const editarDesafio = new EditarDesafioController()
 const removerDesafio = new RemoverDesafioController()
 const criarDesafio = new CriarDesafioController()
+const editarAvaliacao = new EditarAvaliacaoController()
+const removerAvaliacao = new RemoverAvaliacaoController()
 
 //#region Comentarios
 treinadorRouter.post("/posts/:id/comentarios/",verificarAutenticacao, criarComentarioController.handle);
@@ -35,6 +38,11 @@ treinadorRouter.get("/treinos/", verTodosTreinosDosAlunosController.handle);
 treinadorRouter.put("/desafio/:id", editarDesafio.handle);
 treinadorRouter.delete("/desafio/:id", removerDesafio.handle);
 treinadorRouter.post("/desafio/:id", criarDesafio.handle)
+//#endregion
+
+//#region Avaliacoes
+treinadorRouter.put("/avaliacoes/:id", editarAvaliacao.handle);
+treinadorRouter.delete("/avaliacoes/:id", removerAvaliacao.handle);
 //#endregion
 
 export { treinadorRouter };
