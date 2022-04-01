80a2ab85fa23cc9867cb5ff07a582ce4652c07cd
roberto barreto <you@example.com>
2022-04-01 11:34:40 +0100

    Criar gosto done

--- src/api/routes/allRoute.ts
+++ src/api/routes/allRoute.ts
@@ -6,6 +6,7 @@ import { VerTodosPostsController } from "../controllers/all/posts/obter/verTodos
 import { VerTodosPostsUserController } from "../controllers/all/posts/obter/verTodosPostsUserController";
 import { VerInfoPostController } from "../controllers/all/posts/obter/verInfoPostController";
 import { EditarPublicacaoController } from "../controllers/all/posts/editarPublicacaoController";
+import { CriarGostoController } from "../controllers/all/gostosPosts/criarGostoController";
 
 const allRouter = express.Router();
 
@@ -16,6 +17,8 @@ const removerPostsController = new RemoverPostController();
 const verTodosPostsUserController = new VerTodosPostsUserController();
 const verInfoPostInfoController = new VerInfoPostController();
 const editarPublicacaoController  = new EditarPublicacaoController();
+const criarGostoController=new CriarGostoController();
+
 
 
 //#region Publicacoes
@@ -24,6 +27,8 @@ allRouter.get("/posts", verTodosPostsController.handle);
 allRouter.get("/posts/user/:id", verTodosPostsUserController.handle)
 allRouter.put("/posts", editarPublicacaoController.handle);
 allRouter.get("/posts/:id", verInfoPostInfoController.handle)
+allRouter.get("/posts/:id/gostos", criarGostoController.handle)
+
 //ver info de post
 
 
