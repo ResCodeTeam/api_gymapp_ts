927613890b8ebc4c7dd91c8fd6f899a489ce5cae
“EngenheiroHenrique” <a21122@alunos.ipca.pt>
2022-04-07 21:32:38 +0100

    .

--- src/api/services/treinador/perfil/editarPerfilService.ts
+++ src/api/services/treinador/perfil/editarPerfilService.ts
@@ -3,29 +3,57 @@ import { client } from "../../../prisma/client";
 
 
 interface IEditarPerfil{
-
-    email: string
-    nome:  string
-    password: string
-    data_nasc: Date 
-    hashtag: String  
-    data_entrada: Date 
-    funcao_id: String
-    refresh_token: String
-    genero: number
-    pontos: number
-    descricao: String
-    imagem_url: string  
+    uId:string,
+    email: string,
+    nome:  string,
+    password: string,
+    dataNasc: Date,  
+    dataEntrada: Date, 
+    funcaoId: String,
+    genero: number,
+    pontos: number,
+    descricao: String,
+    estado:number,
+    imagemUrl: string  
 }
+
 
 export class EditarPerfilService {
-    async execute(userId: string) {
-      const exists_dst = await checkUserIdExists(userId);
-      if (!exists_dst) {
-        throw new Error("A modalidade não existe");
-      }
-    }
-}
+  async execute({  
+    uId,
+    email,
+    nome,
+    password,
+    dataNasc,
+    dataEntrada,
+    funcaoId,
+    genero,
+    pontos,
+    descricao,
+    estado,
+    imagemUrl 
 
+  } : IEditarPerfil){
+    const editarModalidades = await client.users.updateMany({
+        where : {
+          uid:uId
+              
+        },
+        data : {
+          email:email,
+          nome:nome,
+          password:password,
+          data_nasc:dataNasc,
+          data_entrada:dataEntrada,
+          genero:genero,
+          pontos:pontos,
+          descricao:descricao,
+          estado:estado,
+          imagem_url:imagemUrl
+        }
+    })
+  }
+
+  
 
 
