aaa4948a14ba983029af61cf7e5c5f496910a1fa
João Castro <joaocastro2002@gmail.com>
2022-04-08 22:16:01 +0100

    to fix ver atividades

--- src/api/routes/backendRoute.ts
+++ src/api/routes/backendRoute.ts
@@ -7,6 +7,7 @@ import { CriarAtividadeController } from "../controllers/atividades/criarAtivida
 // import { RemoverMarcaService } from "../services/marcas/removerMarcaService";
 import { RemoverAtividadesController } from "../controllers/atividades/removerAtividadesController";
 import { EditarAtividadesController } from "../controllers/atividades/editarAtividadesController";
+import { VerTodasAtividadesController } from "../controllers/atividades/verTodasAtividadesController";
 
 
 const registarFuncoesController = new RegistarFuncoesController();
@@ -14,6 +15,7 @@ const registarCpController = new RegistarCpController();
 const criarAtividadeController = new CriarAtividadeController();
 const removerAtividadesController = new RemoverAtividadesController();
 const editarAtividadesController = new EditarAtividadesController;
+const verTodasAtividadesController = new VerTodasAtividadesController();
 
 
 backendRouter.post("/funcoes/", registarFuncoesController.handle);
@@ -21,6 +23,7 @@ backendRouter.post("/cp/", registarCpController.handle);
 backendRouter.post("/atividades/", criarAtividadeController.handle);
 backendRouter.delete("/atividades/:id", removerAtividadesController.handle);
 backendRouter.put("/atividades/:id", editarAtividadesController.handle);
+backendRouter.get("/atividades/:id", verTodasAtividadesController.handle);
 
 export { backendRouter };
 
