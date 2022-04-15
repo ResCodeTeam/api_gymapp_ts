82eda6e1550b373c4bfa54ceff87c66218c67c05
roberto barreto <you@example.com>
2022-04-15 22:52:33 +0100

    verificar treinador na autenticação

---
+++ src/api/middlewares/verificarTreinador.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { decode } from "jsonwebtoken";
+import { checkUserIdExists, getFuncaoId, getUserFuncao } from "../helpers/dbHelpers";
+
+export async function verificarAdmin(request:Request, response:Response, next:NextFunction){
+    const auth = request.headers.authorization;
+
+    const[,token]=auth.split(" ")
+
+    
+    let uid = decode(token)['sub'].toString()
+    
+    const user = checkUserIdExists(uid)
+    if(!user){
+        throw new Error("User inexistente")
+    }
+
+    const funcao_id = await getUserFuncao(uid);
+    const treinador_id = await getFuncaoId("Treinador")
+
+    if(funcao_id==treinador_id){
+        next();
+    }
+    else{
+        throw new Error("Não possui autorização")
+    }
+
+}
