47beb2b16cdf148c50a2612dd2fe0445cd84db71
roberto barreto <you@example.com>
2022-04-17 20:37:11 +0100

    criar local de medida

--- src/api/controllers/localMedida/criarLocalMedidaController.ts
+++ src/api/controllers/localMedida/criarLocalMedidaController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
+import { CriarLocalMedidaService } from "../../services/localMedida/criarLocalMedidaService";
 
 export class CriarLocalMedidaController{
   async handle(request:Request, response:Response){
     const uid = response.locals.uid;
+    const marcaId = request.params.marcaId;
     const{descricao,unilado}=request.body;
     if(descricao===undefined||unilado===undefined){
       throw new Error("Pedido Invalido")
     }
-    
+
+    const criarLocalMedidaService = new CriarLocalMedidaService();
+    const resp = await criarLocalMedidaService.execute(uid,marcaId,descricao,unilado)
+
+    response.json(resp)
   }
 }
--- src/api/services/localMedida/criarLocalMedidaService.ts
+++ src/api/services/localMedida/criarLocalMedidaService.ts
@@ -1,5 +1,42 @@
+import { checkAutorMarca, checkMarcaExists } from "../../helpers/dbHelpers";
+import { client } from "../../prisma/client";
+
 export class CriarLocalMedidaService{
-  async execute(uid:string, descricao:String, unilado:boolean){
+  async execute(uid:string,marcaId:string, descricao:string, unilado:boolean){
     
+    const existMarca = await checkMarcaExists(marcaId);
+    if(!existMarca){
+      throw new Error("Marca inexistente")
+    }
+    
+    
+    const isAutorMarca = await checkAutorMarca(uid,marcaId);
+    if(!isAutorMarca){
+      throw new Error("Não possui autorização para realizar esta operação")
+    }
+
+    const localMedida = await client.locais_medidas.create({
+      data:{
+        descricao,
+        unilado,
+      }
+    })
+    try{
+      await client.local_medidas_marca.create({
+        data:{
+          marca_id:marcaId,
+          local_medida_id:localMedida.local_medida_id
+        }
+      })
+    }catch(e){
+      client.locais_medidas.delete({
+        where:{
+          local_medida_id:localMedida.local_medida_id
+        }
+      })
+    }
+
+    return localMedida
+
   }
 }
