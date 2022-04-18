b8aee82240b1b56c92b83f60d91d8fbd6e09775f
roberto barreto <you@example.com>
2022-04-18 16:52:55 +0100

    fix criar desafios response

--- src/api/services/desafios/criarDesafiosService.ts
+++ src/api/services/desafios/criarDesafiosService.ts
@@ -84,7 +84,73 @@ class CriarDesafiosService {
                 });
             }
         }
-        return desafio;
+
+        const desaf = await client.desafios.findFirst({
+            where:{
+                desafio_id:desafio.desafio_id
+            },
+            select:{
+                desafio_id:true,
+                nome:true,
+                data_inicio:true,
+                data_fim:true,
+                recompensa:true,
+                isEncerrado:true,
+                descricao:true,
+                users:{
+                    select:{
+                        nome:true,
+                        email:true,
+                        imagem_url:true
+                    }
+                },
+                modalidades_ginasio:{
+                    select:{
+                        nome:true
+                    }
+                },
+                regras_desafio:{
+                    select:{
+                        descricao:true
+                    }
+                },
+                exercicios_desafio:{
+                    select:{
+                        n_ordem_exercicio:true,
+                        genero:true,
+                        exercicios:{
+                            select:{
+                                nome:true,
+                                descricao:true,
+                                is_tempo:true,
+                                imagens:{
+                                    select:{
+                                        url:true
+                                    }
+                                },
+                                musculos:{
+                                    select:{ 
+                                        musculos:{
+                                            select:{
+                                                nome:true,
+                                                img_url:true
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        series_desafio:{
+                            select:{
+                                n_ordem_serie:true,
+                                valor:true,
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        return desaf;
     }
 }
 
