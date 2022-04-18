3b5c0f1f83c2b0eee30f7dddf9d423be41df8659
Ângela Silva <31926429+AngelaSilva98@users.noreply.github.com>
2022-04-19 00:24:30 +0100

    Atualização Ver Perfil Service

--- src/api/services/perfil/verPefilService.ts
+++ src/api/services/perfil/verPefilService.ts
@@ -1,7 +1,10 @@
 
 import { checkUserIdExists } from "../../helpers/dbHelpers";
+import { verificarAdminTreinador } from "../../middlewares/verificarAdminTreinador";
 import { client } from "../../prisma/client";
+import { VerTodasAtividadesService } from "../atividades/verTodasAtividadesService";
 import { VerTodosPostsUserService } from "../posts/obter/verTodosPostsUserService";
+import { VerTreinosAlunosService } from "../treinos/verTreinosAlunosService";
 
 export class VerPerfilService{
     async execute(uId:string){
@@ -14,17 +17,32 @@ export class VerPerfilService{
         const perfil = await client.users.findMany({
             where:{
                 uid:uId,
-                isDeleted:false,
-                definicoes_user:{
-                    is_privado:false
+                isDeleted:false
+            },
+            select: {
+                nome: true,
+                hashtag: true,
+                descricao: true,
+                imagem_url: true,
+                definicoes_user: {
+                    select: {
+                        is_privado: true
+                    }
                 }
             }
-                })
-
-                const verTodosPostsUserService = new VerTodosPostsUserService()
-                const posts = await verTodosPostsUserService.execute(uId);
+        });
         
-        return {perfil,posts}
+        if (!perfil[0].definicoes_user.is_privado) {
+            return perfil;
+        }
+
+        const verTodosPostsUserService = new VerTodosPostsUserService();
+        const posts = await verTodosPostsUserService.execute(uId);
+
+        const verTodosTreinosUserService = new VerTreinosAlunosService();
+        const treinos = await verTodosTreinosUserService.execute(uId);
+        
+        return {perfil, posts, treinos}
     }
 }
 
