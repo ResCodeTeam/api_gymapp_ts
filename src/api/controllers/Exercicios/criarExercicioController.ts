04047138c2382687f983584b589193bcfe46f234
roberto barreto <you@example.com>
2022-04-18 15:41:59 +0100

    fix criar exercicio

--- src/api/controllers/Exercicios/criarExercicioController.ts
+++ src/api/controllers/Exercicios/criarExercicioController.ts
@@ -3,8 +3,9 @@ import { CriarExercicioService } from "../../services/exercicios/criarExercicioS
 
 export class CriarExercicioController{
   async handle(request:Request, response:Response){
-    const {nome, descricao, autor,isTempo, imagens, musculos}= request.body;
-    if(nome === undefined || descricao === undefined || autor === undefined || isTempo === undefined || imagens === undefined || musculos === undefined){
+    const autor = response.locals.uid;
+    const {nome, descricao, isTempo, imagens, musculos}= request.body;
+    if(nome === undefined || descricao === undefined || isTempo === undefined || imagens === undefined || musculos === undefined){
       throw new Error("Pedido inválido")
     }
 
