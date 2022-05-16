7e826f168ff56c91a5db2d0f209b94dc3af4b448
roberto barreto <you@example.com>
2022-05-16 13:57:01 +0100

    start of branch

--- src/api/controllers/admin/registarAdminControllers.ts
+++ src/api/controllers/admin/registarAdminControllers.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 import { RegistarAdminService } from "../../services/admin/registarAdminService";
 
-export class RegistarAdminController{
-    async handle (request:Request, response:Response) {
-    let { email,nome,password,dataNasc,dataEntrada,genero } = request.body;
-    if(email === undefined || nome === undefined || password === undefined || dataNasc === undefined || dataEntrada === undefined || genero === undefined){
+export class RegistarAdminController {
+  async handle(request: Request, response: Response) {
+    let { email, nome, password, dataNasc, dataEntrada, genero } = request.body;
+    if (email === undefined || nome === undefined || password === undefined || dataNasc === undefined || dataEntrada === undefined || genero === undefined) {
       throw new Error("Pedido inválido")
     }
-    
+
+
     const registarAdminService = new RegistarAdminService();
     dataNasc = new Date(dataNasc);
     dataEntrada = new Date(dataEntrada);
 
-    const resp = await registarAdminService.execute({email,nome,password,dataNasc,dataEntrada,genero});
-  
+    const resp = await registarAdminService.execute({ email, nome, password, dataNasc, dataEntrada, genero });
+
     response.status(200).json(resp);
-  
+
   }
 }
-  
