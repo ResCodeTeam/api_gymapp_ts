824eea1b8a6903a2a4c4db3c8b04b917039886aa
roberto barreto <you@example.com>
2022-05-16 23:12:14 +0100

    jwt removed

--- src/api/controllers/auth/logoutController.ts
+++ src/api/controllers/auth/logoutController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { LogoutService } from "../../services/auth/logoutService";
 
-export class LogoutController{
-    async handle(request:Request, response:Response){
+export class LogoutController {
+    async handle(request: Request, response: Response) {
         const userId = request.params.userId;
-        const token = response.locals.token;
-        
+        const token = request.headers.authorization.split(' ')[1];
+
         const logoutService = new LogoutService();
-        const resp = await logoutService.execute(userId,token);
+        const resp = await logoutService.execute(userId, token);
         response.json(resp)
     }
 }
